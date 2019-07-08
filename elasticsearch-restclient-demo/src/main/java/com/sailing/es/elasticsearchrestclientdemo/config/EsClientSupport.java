26a2314e3d10649411a66982eec1af5e6a3ce557
rdman <rdman84@126.com.com>
2019-07-08 22:38:37 +0800

    增加es的springboot代码

--- elasticsearch-restclient-demo/src/main/java/com/sailing/es/elasticsearchrestclientdemo/config/EsClientSupport.java
+++ elasticsearch-restclient-demo/src/main/java/com/sailing/es/elasticsearchrestclientdemo/config/EsClientSupport.java
@@ -1,6 +1,10 @@
 package com.sailing.es.elasticsearchrestclientdemo.config;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
 import org.elasticsearch.action.admin.indices.alias.Alias;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
@@ -11,19 +15,31 @@ import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.FieldSortBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -118,7 +134,6 @@ public class EsClientSupport {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-
         }
         return false;
     }
@@ -184,6 +199,83 @@ public class EsClientSupport {
            e.printStackTrace();
         }
         return 0;
+    }
+
+    public static String queryAllByLower(String index,String type){
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("match_all","{}");
+            JSONObject queryObject = new JSONObject();
+            queryObject.put("query",jsonObject);
+            String queryStr = JSON.toJSONString(queryObject);
+            HttpEntity entity = new NStringEntity(queryStr,ContentType.APPLICATION_JSON);
+            String endPoint = "/"+index+"/"+type+"/_search";
+            Response response = client.getLowLevelClient().performRequest("POST", endPoint, Collections.<String, String>emptyMap(), entity);
+            return response.toString();
+        } catch (Exception e) {
+            System.out.println("写入数据失败");
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static String queryPage(String index,String type,
+                                   int start,int size,
+                                   String fieldName,String value,
+                                   String startDate, String endDate){
+
+        // 这个sourcebuilder就类似于查询语句中最外层的部分。包括查询分页的起始，
+        // 查询语句的核心，查询结果的排序，查询结果截取部分返回等一系列配置
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        //查询等待时间
+        sourceBuilder.timeout(new TimeValue(60,TimeUnit.SECONDS));
+        //结果开始处
+        sourceBuilder.from(start);
+        //查询结果结束处
+        sourceBuilder.size(size);
+        //
+        MatchQueryBuilder matchbuilder = QueryBuilders.matchQuery(fieldName,value);
+        //同时满足两个条件
+        matchbuilder.operator(Operator.AND);
+
+        RangeQueryBuilder rangbuilder = QueryBuilders.rangeQuery("date");
+        if(!"".equals(startDate)){
+            rangbuilder.gte(startDate);
+        }
+        if(!"".equals(endDate)){
+            rangbuilder.lte(endDate);
+        }
+        // 等同于bool，将两个查询合并
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(matchbuilder);
+        boolQueryBuilder.must(rangbuilder);
+        //排序
+        FieldSortBuilder fieldSortBuilder = SortBuilders.fieldSort("date");
+        fieldSortBuilder.order(SortOrder.DESC);
+        sourceBuilder.sort(fieldSortBuilder);
+        //
+        sourceBuilder.query(boolQueryBuilder);
+        System.out.println(sourceBuilder);
+        SearchRequest searchRequest = new SearchRequest(index);
+        searchRequest.types(type);
+        searchRequest.searchType(SearchType.QUERY_THEN_FETCH);
+        searchRequest.source(sourceBuilder);
+        SearchResponse response = null;
+        try {
+            response = client.search(searchRequest, RequestOptions.DEFAULT);
+            SearchHits hits = response.getHits();
+            long  totalHits = hits.totalHits;
+//            for(SearchHit searchHit:hits){
+//
+//            }
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+
+
+
     }
 
     /**
