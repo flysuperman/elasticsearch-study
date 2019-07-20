575559df35234429d5b829cd9e3efd8ec4660c64
rdman <rdman84@126.com.com>
2019-07-20 09:59:43 +0800

    真理es 高级客户端代码

---
+++ elasticsearch-restclient-demo/src/main/java/com/sailing/es/elasticsearchrestclientdemo/ElasticsearchRestclientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sailing.es.elasticsearchrestclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchRestclientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchRestclientDemoApplication.class, args);
+    }
+
+}
---
+++ elasticsearch-springboot-test/src/main/java/com/sailing/es/test/ElasticsearchSpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package com.sailing.es.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchSpringbootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchSpringbootTestApplication.class, args);
+    }
+
+}
